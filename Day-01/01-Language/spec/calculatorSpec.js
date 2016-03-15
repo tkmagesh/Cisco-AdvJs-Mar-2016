f453db3f353966f6760e3daa1734f414d92fdd01
tkmagesh <tkmagesh77@gmail.com>
2016-03-15 11:22:01 +0530

    Calculator - Seed

---
+++ Day-01/01-Language/spec/calculatorSpec.js
@@ -0,0 +1,14 @@
+describe("A calculator", function(){
+	it("should be able to add two numbers", function(){
+		//Arrange
+		var n1 = 10,
+			n2 = 20,
+			expectedResult = 30;
+
+		//Act
+		var result = add(n1,n2);
+
+		//Assert
+		expect(result).toBe(expectedResult);
+	});
+});
