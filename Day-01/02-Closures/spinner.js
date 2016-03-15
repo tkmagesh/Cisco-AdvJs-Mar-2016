4a0b7fda6a6a369b074b1996245d3e666e577b52
tkmagesh <tkmagesh77@gmail.com>
2016-03-15 15:41:35 +0530

    Spinner assignment

---
+++ Day-01/02-Closures/spinner.js
@@ -0,0 +1,18 @@
+/* Create an object and assign it to a variable 'spinner' */
+
+var spinner = ....
+
+/*The object created must exhibit the following behavior
+	spinner.up() // => 1
+	spinner.up() // => 2
+	spinner.up() // => 3
+	spinner.up() // => 4
+
+	spinner.down() // => 3
+	spinner.down() // => 2
+	spinner.down() // => 1
+	spinner.down() // => 0
+	spinner.down() // => -1
+
+	THE VARIABLE USED TO TRACK THE VALUE SHOULD NOT BE VISIBLE TO THE OUTSIDE WORLD
+*/
