dfcafc42825ba88b250b4b7afadb6e263243a237
tkmagesh <tkmagesh77@gmail.com>
2016-03-16 09:48:40 +0530

    prime finder - solution

--- Day-01/02-Closures/primeFinder.js
+++ Day-01/02-Closures/primeFinder.js
@@ -1,9 +1,20 @@
 //create a "primeFinder" function that can find if the give number is a prime number or not.
 //The algorithm should not be executed for a given number more than once
 
-function isPrime(n){
-
-}
+var isPrime = (function(){
+	var cache = {};
+	function checkPrime(n){
+		if (n <= 3) return true;
+		for(var i=2; i <= (Math.sqrt(n)); i++)
+			if (n % i === 0) return false;
+		return true;
+	}
+	return function(n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = checkPrime(n);
+		return cache[n];
+	}
+})();
 
 isPrime(100) //=> run the algo
 isPrime(101) //=> run the algo
@@ -14,3 +25,4 @@ isPrime(100) //=> DO NOT run the algo
 isPrime(101) //=> DO NOT run the algo
 isPrime(102) //=> DO NOT run the algo
 isPrime(103) //=> DO NOT run the algo
+
