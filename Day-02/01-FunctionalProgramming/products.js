48ac6da6a5f1dfe9d48acf8a12b0264e0601d629
tkmagesh <tkmagesh77@gmail.com>
2016-03-16 11:32:36 +0530

    Sorting

--- Day-02/01-FunctionalProgramming/products.js
+++ Day-02/01-FunctionalProgramming/products.js
@@ -30,16 +30,59 @@ describe("Default list", function(){
 
 describe("Sorting", function(){
 	describe("Default sorting [products by id]", function(){
-		//sort();
+		function sort(){
+			for(var i=0; i<products.length-1; i++)
+				for(var j=i+1; j<products.length; j++)
+					if (products[i].id > products[j].id){
+						var temp = products[i];
+						products[i] = products[j];
+						products[j] = temp;
+					}
+		}
+		sort();
 		console.table(products);
 	});
 
-	describe("sorting any list by any attribute [products by cost]", function(){
-		//sort();
-		console.table(products);
+	describe("sorting any list by any attribute", function(){
+		function sort(list, attrName){
+			for(var i=0; i<list.length-1; i++)
+				for(var j=i+1; j<list.length; j++)
+					if (list[i][attrName] > list[j][attrName]){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+		}
+		describe("Products by cost", function(){
+			sort(products, "cost");
+			console.table(products);
+		});
+		describe("Products by units", function(){
+			sort(products, "units");
+			console.table(products);
+		})
 	});
-	describe("sorting any list by any comparison [ products by value(units * cost)", function(){
-		//sort();
-		console.table(products);
+	describe("sorting any list by any comparer", function(){
+		function sort(list, comparerFn){
+			for(var i=0; i<list.length-1; i++)
+				for(var j=i+1; j<list.length; j++)
+					if (comparerFn(list[i], list[j]) > 0 ){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+		}
+		describe("Products by value (units * cost)", function(){
+			var productsComparerByValue = function(p1, p2){
+				var p1Value = p1.cost * p1.units,
+					p2Value = p2.cost * p2.units;
+				if (p1Value < p2Value) return -1;
+				if (p1Value === p2Value) return 0;
+				return 1;
+			};
+			sort(products, productsComparerByValue);
+			console.table(products);
+		});
+		
 	});
 });
